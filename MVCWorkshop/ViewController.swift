d1d9061c6fe35cf1dc7fb70af67255074a8eeda0
Macintosh <macintosh@yomacbook-pro>
2016-05-01 14:31:44 +0700

    Initial Commit

---
+++ MVCWorkshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVCWorkshop
+//
+//  Created by Macintosh on 5/1/2559 BE.
+//  Copyright © 2559 Macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
